f8bef952c12ce8eed8d47e80353b7c63140283f9
nilupulmanodya <n.manodya@gmail.com>
2022-01-15 17:01:37 +0530

    inital template with chatbot interface

---
+++ frontend/demo-uni/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomChatbot from "./components/chatbot/CustomChatbot";
+
+function App() {
+  return (
+  
+		<CustomChatbot />
+      
+    
+  );
+}
+
+export default App;
